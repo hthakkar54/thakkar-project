7c4545959c1375b1fa7a9a2bb9e0a6aee22591c3
Harsh Thakkar <hthakkar54@yahoo.com>
2017-09-26 18:24:12 +0000

    Root to users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
-Rails.application.routes.draw do
+ Rails.application.routes.draw do
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#index'
+ end
 
-root 'application#hello'
-
-end
